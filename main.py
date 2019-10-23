6fb59c55e31836d5ac5140cecf7727c91b1f0b6a
4tal <idan.4tal@gmail.com>
2019-10-23 23:32:08 +0300

    automatic commit

--- main.py
+++ main.py
@@ -1,6 +1,60 @@
 import sys
+import os
 
-print(f'This is the name of the script: {sys.argv[0]}')
-print(f'Number of arguments: {len(sys.argv[0])}')
-print(f'The arguments are: {str(sys.argv)}')
+#TODO: implement automatic setup.
+#TODO: get the path of my folder.
+#TODO: add configuration file.
+#TODO: Hierarchical getters and setups.
 
+
+commands = {
+        "git push": ['cd ~/Code/jack/tools ;git add .;git commit -m "automatic commit";git push']
+    }
+
+options = {
+    "pipenv": [
+        "pipenv install <package>",
+        "pipenv update <package>",
+        "pipenv install"
+        ],
+    "git": [
+        "git reset --hard <commit>",
+        "git stats",
+        "git add <file/.>"
+    ],
+    "vim": [
+        ':wq -> save and exit',
+        ':q! -> exit without save'
+    ],
+    "linux": ""
+    }
+
+setups = {
+    "git":["git init"],
+    "ssh":{
+        "generate":["""ssh-keygen -t rsa -C 'idan.4tal@gmail.com'"""]
+    }
+}
+
+
+def main():
+    if len(sys.argv) == 1:
+        for option in options:
+            print(f'{option}')
+        return
+    
+    if sys.argv[1]=='setup':
+        for setup in setups:
+            print(f'{setup}')
+        return
+
+    if sys.argv[1]=='command':
+        for command in commands:
+            print(f'{command}')
+        choosen_command = str(input("Choose command."))
+        for command in commands[choosen_command]:
+            os.system(command)
+        return
+    
+    
+main()
